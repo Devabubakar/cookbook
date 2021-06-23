58abaa0d7a97475c73dc8ef832fec3a2169bc01f
Abubakar Ali <abubakarali3w@gmail.com>
2021-06-23 14:03:43 +0300

    added react-routerr to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-
+import { Switch, Route } from 'react-router-dom';
+import  Homepage  from './pages/homepage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Switch>
+        <Route exact to='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default Homepage
