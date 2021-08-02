585f6a2688674ecab20fc2f26bb47cea491566d5
Devabubakar <abubakarali3w@gmail.com>
2021-08-03 00:23:58 +0300

    added bookmark icon placeholder for when bookmark is empty

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography, Box } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { bookmarksSelector } from '../redux/bookmark/selectors';
 import MediaCard from './card';
+import { ReactComponent as EmptyIcon } from './assets/empty.svg';
 
 const Bookmark = ({ meals }) => {
   return (
     <div>
       <Grid container direction='row' alignItems='center' spacing={4}>
-        {meals.map((meal) => (
-          <Grid item key={meal.idMeal} xs={12} sm={6} md={4} lg={3}>
-            <MediaCard meal={meal} />
-          </Grid>
-        ))}
+        {/*Check if the bookmark Array is empty*/}
+        {meals.length > 0 ? (
+          meals.map((meal) => (
+            <Grid item key={meal.idMeal} xs={12} sm={6} md={4} lg={3}>
+              <MediaCard meal={meal} />
+            </Grid>
+          ))
+        ) : (
+          <Box
+            display='flex'
+            textAlign='center'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+            width='100vw'
+          >
+            <Box>
+              <EmptyIcon style={{ height: '60vh', width: '100%' }} />
+            </Box>
+            <Box>
+              <Typography
+                variant='h5'
+                color='primary'
+                align='center'
+                gutterBottom
+              >
+                Empty Bookmark 🙃
+              </Typography>
+              <Typography variant='h5' color='primary' align='center'>
+                Bookmark a Recipe and It will show up here.
+              </Typography>
+            </Box>
+          </Box>
+        )}
       </Grid>
     </div>
   );
